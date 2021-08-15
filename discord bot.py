f5223f83134d6f8938d6bcf572613e80eb4ef33c
salihdursun1 <88969058+salihdursun1@users.noreply.github.com>
2021-08-15 14:16:04 +0300

    Add files via upload

---
+++ discord bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext.commands import Bot
+
+TOKEN = "<discordtoken>"
+
+client = discord.Client()
+bot = Bot(command_prefix="!")
+
+
+@bot.event
+async def on_ready():
+    print("Bot Hazır " + str(bot.user))
+
+
+@bot.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == "selam":
+        await message.channel.send("selam naber")
+
+
+bot.run(TOKEN)
